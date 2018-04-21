5c8df2620b1788dbbd31da6c1a864144600bdbb9
starfy84 <dereck.tu@student.tdsb.on.ca>
2018-04-21 00:35:33 -0400

    __repr__ and __str__ added

--- DMOBPlayer.py
+++ DMOBPlayer.py
@@ -12,13 +12,19 @@ class DMOBPlayer:
 		s.write(str(self.__dict__).replace("'","\""))
 		s.close()
 
+	def __repr__(self):
+		return "DMOBPlayer('{}','{}','{}','{}','{}')".format(self.name,self.discord_id,self.points,self.rank,self.language)
+
+	def __str__(self):
+		return "Name: {}, Points: {}, Rank: {}".format(self.name,self.points,self.rank)
+
 	@staticmethod
 	def read(name):
 		try:
 			f = open("players\\"+name+".json","r")
 			d = json.loads(f.read())
 			f.close()
+			return DMOBPlayer(d["name"],d["discord_id"],d["points"],d["rank"],d["language"])
 		except FileNotFoundError:
 			print("File Not Found")
-		return DMOBPlayer(d["name"],d["discord_id"],d["points"],d["rank"],d["language"])
-
+		

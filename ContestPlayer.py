08ba6f1b63c61d06c1f45700c960381a4e2c8eee
Evan Zhang <Ninjaclasher@users.noreply.github.com>
2018-05-25 14:02:42 -0400

    Delete ContestPlayer.py

--- ContestPlayer.py
+++
@@ -1,7 +0,0 @@
-class ContestPlayer:
-    def __init__(self,user,num_problems):
-        self.user=user
-        self.problems=[0]*num_problems
-
-    def __eq__(self,other):
-        return self.user == other.user
